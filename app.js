93943bce2c1c50f582cfe8dad909afb1a4ea02d5
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-06-15 16:34:17 -0700

    Create item and display company info

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var stickyApp = angular.module('stickyApp', ['firebase']);
 var sticky = sticky || {};
 sticky.firebase = new Firebase("stickyapp.firebaseIO.com");
 sticky.firebaseCompany = new Firebase("stickyapp.firebaseIO.com/companies");
+sticky.firebaseCompanyRef = function(companyId) {
+	return new Firebase("stickyapp.firebaseIO.com/companies/" + companyId);
+}
 
 stickyApp.config(function($routeProvider) {
   $routeProvider. 
   when("/", {controller: "HomeCtrl", templateUrl: "home.html"}).
   when("/profile", {controller: "ProfileCtrl", templateUrl: "profile.html"}).
   when("/dashboard", {controller: "DashboardCtrl", templateUrl: "dashboard.html"}).
-  when("/company_create", {controller: "CompanyCtrl", templateUrl: "company.html"});
+  when("/company/create", {controller: "CompanyCtrl", templateUrl: "company/company_create.html"}).
+  when('/company/:companyId', {controller: "CompanyCtrl", templateUrl: "company/company_show.html"});
 });
--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,13 +1,31 @@
 var sticky = sticky || {};
 
 
-stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, angularFireCollection) {
+stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, $routeParams, angularFire) {
+
+  $scope.companyId = $routeParams.companyId;
+
+  $scope.company = angularFire(sticky.firebaseCompanyRef($scope.companyId), $scope, "company");
+
+  console.log($scope.company.then());
 
   $scope.createCompany = function() {
-    sticky.firebaseCompany.set(
+    sticky.firebaseCompany.push(
       {
         name: "Chef Xiao",
         year: "",
       });
-  }  
+  };
+
+  $scope.createItem = function() {
+    if ($scope.companyId) {
+      sticky.firebase.companyRef($scope.companyId).push(
+        {
+          name: "Mapo Tofu",
+          price: 12,
+          votes: 0
+        });
+    }
+  };
+
 });
