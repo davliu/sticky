fe2afa9e58968160d97461610bb4a283c013c732
clydeli <clyde1008li@gmail.com>
2013-06-16 09:59:32 -0700

    try to deal with resize

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,20 @@ $(document).on('ready', function(){
   $('#main-menu > ul > li').click(function(){
     $('#main-menu > ul > li').removeClass('active');
     $(this).addClass('active');
-  })
+  });
 
   $('#mobile-menu .menu-icon a').click(function(e){
     $('#mobile-menu').toggleClass('expanded');
     e.preventDefault();
-  })
+  });
 
   $('#mobile-menu .top-bar-section').click(function(e){
     $('#mobile-menu').toggleClass('expanded');
     //e.preventDefault();
-  })
+  });
+
+  $(window).resize(function() {
+    $('.company-box').css('height', $('.company-box').width()+10+'px');
+  });
 
 });
