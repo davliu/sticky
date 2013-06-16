283f59b58112f7ad41201d3e64bf093d361daeec
David Liu <dliu882000@gmail.com>
2013-06-16 03:56:24 -0700

    prettied up graph

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -10,6 +10,9 @@ stickyApp.controller('DashboardCtrl', function DashboardCtrl($scope, angularFire
         $scope.filteredCompetitions.push(competition);
       }
     });
+
+    $scope.selectedIndex = 0;
+    $scope.renderCharts(0);
   });
 
   $scope.renderCharts = function(index) {
@@ -17,67 +20,39 @@ stickyApp.controller('DashboardCtrl', function DashboardCtrl($scope, angularFire
 
     var stats = $scope.filteredCompetitions[index].statistics;
 
+    var chartOptions = {
+      scaleOverride: true,
+      scaleStartValue: 0,
+      scaleSteps: 10,
+      scaleStepWidth: 5
+    };
+
     // Age graph
-    var statLabels = [];
-    var statValues = [];
-    angular.forEach(stats.age, function(value, label) {
-      statLabels.push(label);
-      statValues.push(value);
+    ['age', 'gender', 'location'].forEach(function(chartName) {
+      var statLabels = [];
+      var statValues = [];
+      var largestValue = 0;
+      angular.forEach(stats[chartName], function(value, label) {
+        statLabels.push(label);
+        statValues.push(value);
+        largestValue = Math.max(largestValue, value);
+      });
+
+      chartOptions.scaleStepWidth = Math.ceil(largestValue/20);
+      chartOptions.scaleSteps = largestValue/chartOptions.scaleStepWidth + 1;
+
+      var barChartData = {
+        labels : statLabels,
+        datasets : [
+          {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            data : statValues
+          }
+        ]
+      }
+      var myLine = new Chart(document.getElementById(chartName + "Canvas").getContext("2d")).Bar(barChartData, chartOptions);
     });
-
-    var barChartData = {
-      labels : statLabels,
-      datasets : [
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          data : statValues
-        }
-      ]
-    }
-    var myLine = new Chart(document.getElementById("ageCanvas").getContext("2d")).Bar(barChartData);
-
-    // Gender graph
-    var statLabels = [];
-    var statValues = [];
-    angular.forEach(stats.gender, function(value, label) {
-      statLabels.push(label);
-      statValues.push(value);
-    });
-
-    var barChartData = {
-      labels : statLabels,
-      datasets : [
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          data : statValues
-        }
-      ]
-    }
-    console.log(statLabels);
-    console.log(statValues);
-    var myLine = new Chart(document.getElementById("genderCanvas").getContext("2d")).Bar(barChartData);
-
-    // Location graph
-    var statLabels = [];
-    var statValues = [];
-    angular.forEach(stats.location, function(value, label) {
-      statLabels.push(label);
-      statValues.push(value);
-    });
-
-    var barChartData = {
-      labels : statLabels,
-      datasets : [
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          data : statValues
-        }
-      ]
-    }
-    var myLine = new Chart(document.getElementById("locationCanvas").getContext("2d")).Bar(barChartData);
   };
 
 });
