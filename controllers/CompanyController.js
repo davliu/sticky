bc22e27f23435c21fa9438d568283c070ea45923
Owen Chu <owenchu@gmail.com>
2013-06-15 23:11:23 -0700

    List ranks and votes in company page.

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -2,7 +2,7 @@ var sticky = sticky || {};
 
 stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, $routeParams, angularFire) {
   $scope.companyId = parseInt($routeParams.companyId, 10);
-  $scope.joinedCompetitions = [];
+  $scope.competitionInfo = [];
   $scope.companyIds = [];
   $scope.competitions = [];
 
@@ -24,16 +24,18 @@ stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, $routeParams, a
 
       if (competition.type === "company" &&
           competition.competitors.indexOf($scope.companyId) !== -1) {
-        $scope.joinedCompetitions.push(competition);
+        $scope.competitionInfo.push({competition: competition,
+                                     info: getCompanyCompetitionInfo(competition, $scope.companyId)});
       }
       else if (competition.type === 'items' &&
-                 competition.competitors.length > 0) {
+               competition.competitors.length > 0) {
         (function() {
-          var itemId = competition.competitors[0];
+          var itemId = getWinningItemId(competition);
           var tmpCompetition = competition;
           angularFire(sticky.firebaseUrl("items/" + itemId), $scope, "item", {}).then(function(item) {
             if (item.company === $scope.companyId) {
-              $scope.joinedCompetitions.push(tmpCompetition);
+              $scope.competitionInfo.push({competition: tmpCompetition,
+                                           info: getItemCompetitionInfo(tmpCompetition, itemId, item)});
             }
           });
         })();
@@ -41,13 +43,44 @@ stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, $routeParams, a
     }
   });
 
-  /*
-  $scope.createCompany = function() {
-    $scope.companies.push(
-    {
-      name: "Chef Xiao",
-      year: "",
-    });
-  };
-  */
+  function getCompanyCompetitionInfo(competition, companyId) {
+    var index = competition.competitors.indexOf(companyId);
+    var votes = competition.votes[index];
+    var rank = 1;
+    var i;
+
+    for (i=0; i<competition.votes.length; ++i) {
+      if (i == index) continue;
+      if (competition.votes[i] > votes) ++rank
+    }
+
+    return {rank: rank, votes: votes};
+  }
+
+  function getItemCompetitionInfo(competition, winningItemId, winningItem) {
+    var index = competition.competitors.indexOf(winningItemId);
+    return {itemName: winningItem.name, votes: competition.votes[index]};
+  }
+
+  function getWinningItemId(competition) {
+    var winningItemId;
+    var votes;
+    var i;
+
+    if (competition.length <= 0) {
+      return null;
+    }
+
+    winningItemId = competition.competitors[0];
+    votes = competition.votes[0];
+
+    for (i=1; i<competition.competitors.length; ++i) {
+      if (competition.votes[i] > votes) {
+        winningItemId = competition.competitors[i];
+        votes = competition.votes[i];
+      }
+    }
+
+    return winningItemId;
+  }
 });
