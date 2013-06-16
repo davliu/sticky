c3468ced1025117a260a7afa36e2678775949d67
Kate Liu <kateycliu@gmail.com>
2013-06-15 22:08:21 -0700

    fix login error

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,63 +1,57 @@
+var loggedIn = false;
+
 function IndexCtrl($scope) {
-    $scope.idStatus = false;
     $scope.getIdStatus = function () {
-        return $scope.idStatus?"in/up":"out";
-    };
-
-    $scope.init = function () {
-        var token = null;
-        intel.auth.init({
-                client_id: '0aea680f877fa6f6beeb5e0024ab805e',
-                secret_id: 'ca9d0b745b17be46',
-                scope: "user:details"
-            },
-            successCallback = function (data) {
-                token = data.access_token;
-                data.access_token.intel.auth.setAccessToken(data.access_token, onSetTokenSuccessCallback, onSetTokenErrorCallback);
-                console.log("init succeeded");
-            },
-            errorCallback = function (error) {
-                console.log("init failed");
-                console.log(error);
-            }
-        );
-    };
-
-    $scope.login = function () {
-        intel.auth.login({
-//        provider : "",
-//        name: "myLoginWindow",
-//        specs: "location=1,status=1,scrollbars=1,width=900,height=600",
-            redirectUri: "http://localhost:8000/WebstormProjects/sticky/#/",
-            locale: "en"
-        }, function(data) {
-            console.log("login succeeded");
-            console.log(data);
-        }, function(error) {
-            console.log("login failed");
-            console.log(error);
-        });
-    };
-
-    $scope.logout = function () {
-        intel.auth.logout(
-            successCallback = function() {
-                console.log("logout succeed")
-            },
-            errorCallback = function () {
-                console.log("logout fail")
-            }
-        );
-    };
-
-    $scope.switchIdStatus = function () {
-        if ($scope.idStatus) {
-            $scope.logout();
-            $scope.idStatus = false;
-        }
-        else {
-            $scope.login();
-            $scope.idStatus = true;
-        }
+        return loggedIn?"out":"in";
     };
 }
+
+function login() {
+    var token = null;
+    intel.auth.init(
+        {
+            client_id: '0aea680f877fa6f6beeb5e0024ab805e',
+            secret_id: 'ca9d0b745b17be46',
+            scope: "user:details"
+        },
+        successCallback = function (data) {
+            console.log("init succeed");
+        },
+        errorCallback = function (data) {
+            console.log("init error");
+        }
+    );
+    intel.auth.login({
+        provider : "",
+        name: "myLoginWindow",
+        specs: "location=1,status=1,scrollbars=1,width=10,height=10",
+        redirectUri: "http://localhost:8000/WebstormProjects/sticky/login.html",
+        locale: "en"
+    }, function(data) {
+        console.log("login succeeded");
+    }, function(error) {
+        console.log("login failed");
+    });
+}
+
+function logout () {
+    intel.auth.logout(
+        successCallback = function() {
+            console.log("logout succeed")
+        },
+        errorCallback = function () {
+            console.log("logout fail")
+        }
+    );
+};
+
+function switchIdStatus () {
+    if (loggedIn) {
+        logout();
+        loggedIn = false;
+    }
+    else {
+        login();
+        loggedIn = true;
+    }
+};
