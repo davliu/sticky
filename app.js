7db725f77865b94b423272e7ebab9605ff81a4d9
Owen Chu <owenchu@gmail.com>
2013-06-15 17:41:38 -0700

    Insert objects to db via angularFire.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var stickyApp = angular.module('stickyApp', ['firebase']);
 var sticky = sticky || {};
 sticky.firebase = new Firebase("stickyapp.firebaseIO.com");
-sticky.firebaseCompany = new Firebase("stickyapp.firebaseIO.com/companies");
+sticky.firebaseCompaniesUrl = "https://stickyapp.firebaseIO.com/companies";
 sticky.firebaseCompanyUrl = function(companyId) {
 	return "https://stickyapp.firebaseIO.com/companies/" + companyId;
 }
@@ -13,5 +13,6 @@ stickyApp.config(function($routeProvider) {
   when("/dashboard", {controller: "DashboardCtrl", templateUrl: "dashboard.html"}).
   when("/company/create", {controller: "CompanyCtrl", templateUrl: "company/company_create.html"}).
   when('/company/:companyId', {controller: "CompanyCtrl", templateUrl: "company/company_show.html"}).
+  when("/company", {controller: "CompanyCtrl", templateUrl: "company/company_show_all.html"}).
   when("/vote/:competitionName", {controller: "VoteCtrl", templateUrl: "vote.html"});
 });
--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,27 +1,16 @@
 var sticky = sticky || {};
 
-
 stickyApp.controller('CompanyCtrl', function CompanyCtrl($scope, $routeParams, angularFire) {
   $scope.companyId = $routeParams.companyId;
 
-  angularFire(sticky.firebaseCompanyUrl($scope.companyId), $scope, "company", {});
+  angularFire(sticky.firebaseCompanyUrl($scope.companyId), $scope, "company");
+  angularFire(sticky.firebaseCompaniesUrl, $scope, "companies");
 
   $scope.createCompany = function() {
-    sticky.firebaseCompany.push(
-      {
-        name: "Chef Xiao",
-        year: "",
-      });
-  };
-
-  $scope.createItem = function() {
-    if ($scope.companyId) {
-      sticky.firebase.companyRef($scope.companyId).push(
-        {
-          name: "Mapo Tofu",
-          price: 12,
-          votes: 0
-        });
-    }
+    $scope.companies.push(
+    {
+      name: "Chef Xiao",
+      year: "",
+    });
   };
 });
