2bfae563276b962d6e31517c80b3169344b1f8a4
Kate Liu <kateycliu@gmail.com>
2013-06-16 12:11:40 -0700

    get user details

--- controllers/CouponController.js
+++ controllers/CouponController.js
@@ -3,7 +3,8 @@ var sticky = sticky || {};
 stickyApp.controller('CouponCtrl', function CouponCtrl($scope, $routeParams, angularFire, $http, flash) {
   
   $scope.flash = flash;
-
+  $scope.email = "";
+  $scope.username = "";
   angularFire(sticky.coupons, $scope, "coupons").then(function(coupons) {
     $scope.coupons = coupons;
   });
@@ -18,4 +19,65 @@ stickyApp.controller('CouponCtrl', function CouponCtrl($scope, $routeParams, ang
         alert("Finish Saving Coupon to EverNote");
     });
   };
+    $scope.initialize1 = function () {
+//        var initOptions = {
+//            client_id: '0aea680f877fa6f6beeb5e0024ab805e',
+//            secret_id: 'ca9d0b745b17be46',
+//            scope: "user:details user:scope profile:full profile:full:write"
+//        };
+    //        intel.auth.init(initOptions,
+    //                initSuccessCallback,
+    //                errorCallback
+    //        );
+    intel.auth.init({
+        client_id: '0aea680f877fa6f6beeb5e0024ab805e',
+        secret_id: 'ca9d0b745b17be46',
+        scope:     "user:details user:scope profile:full profile:full:write"
+        },
+    success = function ()
+                {
+                    console.log("init ok");
+                    },
+    error = function() {
+        console.log("init error");
+        }
+
+    );
+
+    intel.auth.login({
+        redirect_uri: "urn:intel:identity:oauth:oob:async",
+//            redirect_uri: "http://localhost:8000/WebstormProjects/sticky/index.html#/",
+        provider: "",
+        name: "myLoginWindow"
+//            specs: "location=1,status=1,scrollbars=1,width=700,height=400"
+        },
+    success = function (userDetails)
+                {
+                    intel.user.getDetails(
+                        successCallback1= function(userDetails)
+                        {
+                            console.log(userDetails.emails[0]);
+                            $scope.email = userDetails.emails[0];
+                            console.log(userDetails.accounts[0].userName);
+                            $scope.username = userDetails.accounts[0].userName;
+                        },
+    errorCallback1 = function(userdetails)
+                    {
+                        alert("failed getting data");
+                        });
+
+
+    },
+    error = function() {
+        console.log("login error");
+        }
+
+    );
+    }
+
+    $scope.initialize1();
+
 });
+
+
+
